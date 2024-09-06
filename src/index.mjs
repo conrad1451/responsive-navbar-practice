48dd1a86e74f9d3736158344945687ecdb1ff508
Conrad Hansen-Quartey <Chansenq@gmail.com>
2024-09-06 13:02:58 +0000

    Initial commit

---
+++ src/index.mjs
@@ -0,0 +1,14 @@
+// import "./styles.css";
+
+// document.getElementById("app").innerHTML = `
+// <h1>Hello JavaScript!</h1>
+// `;
+function classToggle() {
+  const navs = document.querySelectorAll(".Navbar__Items");
+
+  navs.forEach((nav) => nav.classList.toggle("Navbar__ToggleShow"));
+}
+
+document
+  .querySelector(".Navbar__Link-toggle")
+  .addEventListener("click", classToggle);
